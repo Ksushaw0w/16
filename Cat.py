257213b57c888ebca7c16cbd6987142f9ff08161
Ksusha <livksusha@gmail.com>
2022-04-27 12:18:56 +0700

    added new file

---
+++ Cat.py
@@ -0,0 +1,7 @@
+from classCat import Cat
+
+Baron = Cat('Барон', 'мальчик', 2)
+Sam = Cat('Сэм', 'мальчик', 2)
+
+print(Baron.getName()+, Baron.getGender(), Baron.getAge())
+print(Sam.getName(), Sam.getGender(), Sam.getAge())
